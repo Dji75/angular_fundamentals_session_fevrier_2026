01a52b4c8b7750bd9ef3172297a58873d0798cc6
Jérôme AUDOUY <jerome.audouy.external@airbus.com>
2026-02-10 17:44:30 +0100

    feat: handling quantity per product (using array, no mutation)

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,15 +1,30 @@
 import { computed, Injectable, signal } from '@angular/core';
 import { Product } from '../models/product';
 
+interface ProductWithQuantity {
+  product: Product;
+  quantity: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  readonly #cart = signal<Product[]>([]);
+  readonly #cart = signal<ProductWithQuantity[]>([]);
 
   readonly nbItemsInCart = computed(() => this.#cart().length);
 
   addToCart(product: Product) {
-    this.#cart.set([...this.#cart(), product]);
+    const foundProductIndex = this.#cart().findIndex((i) => i.product.id == product.id);
+    if (foundProductIndex < 0) {
+      this.#cart.set([ ...this.#cart(), { product, quantity: 1 }]);
+    } else {
+      this.#cart.set([
+        ...this.#cart().slice(0, foundProductIndex),
+        { product, quantity: this.#cart()[foundProductIndex].quantity + 1 },
+        ...this.#cart().slice(foundProductIndex + 1),
+      ]);
+    }
+
   }
 }
